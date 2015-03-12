a381fa1cbbd3524e0c50c2dd21bd6515d41c0d3a
Mikko Lehtinen <m@mikkolehtinen.com>
2015-03-12 17:00:07 +0200

    more tests

--- test/test.indexing.db.local.js
+++ test/test.indexing.db.local.js
@@ -3,9 +3,9 @@ require('chai').should();
 
 describe('IndexingDBLocal tests', function() {
   var collection = new setup.TestIndexedCollection();
+  var model = new setup.MyModel({id: 'foo'});
 
   it('should add a model to index', function() {
-    var model = new setup.MyModel({id: 'foo'});
     return collection.addToIndex(model);
   });
 
@@ -14,8 +14,50 @@ describe('IndexingDBLocal tests', function() {
       .readFromIndex()
       .then(function() {
         collection.length.should.equal(1);
-        var model = collection.at(0);
-        model.id.should.equal('foo');
+        var m = collection.at(0);
+        m.id.should.equal('foo');
       });
   });
+
+  it('should check if model exists', function() {
+    return collection
+      .exists(model)
+      .then(function(exists) {
+        exists.should.equal(true);
+      });
+  });
+
+  it('should get count', function() {
+    return collection
+      .count()
+      .then(function(count) {
+        count.should.equal(1);
+      });
+  });
+
+  it('should throw if getting score for non-sorted collection', function() {
+    var score = function() {
+      collection.score(model);
+    };
+    score.should.throw();
+  });
+
+  it('should remove model from index', function() {
+    return collection
+      .removeFromIndex(model);
+  });
+
+  it('should get count after deletion', function() {
+    return collection
+      .count()
+      .then(function(count) {
+        count.should.equal(0);
+      });
+  });
+
+  it('should remove the index', function() {
+    return collection.destroyAll();
+  });
+
+
 });
